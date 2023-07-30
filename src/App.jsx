4fe084c8798dd4b47302882c8466164f131a0138
Uttam Makwana <uttamakwana4503@gmail.com>
2023-07-30 20:20:37 +0530

    main.jsx, App.jsx and AllRoutes.jsx files are clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 import { useEffect, useState } from "react";
 import AllRoutes from "./pages/Routes/AllRoutes";
 
 function App() {
+  // to toggle theme
   const [theme, setTheme] = useState("light");
   useEffect(() => {
+    // for light mode
     if (theme === "light") {
       document.documentElement.style.setProperty("--sixty-pr-color", "#f1f1f1");
       document.documentElement.style.setProperty(
@@ -14,20 +16,22 @@ function App() {
       );
       document.documentElement.style.setProperty("--ten-pr-color", "#545AE7");
       document.documentElement.style.setProperty("--shade", "#d7dcf8");
-    } else {
-      document.documentElement.style.setProperty("--sixty-pr-color", "#2C1F4A");
-      document.documentElement.style.setProperty("--thirty-pr-color", "#fff");
-      document.documentElement.style.setProperty("--ten-pr-color", "#DBC4F0");
-      document.documentElement.style.setProperty("--shade", "#3a2b5c");
     }
+    // else {
+    //   document.documentElement.style.setProperty("--sixty-pr-color", "#2C1F4A");
+    //   document.documentElement.style.setProperty("--thirty-pr-color", "#fff");
+    //   document.documentElement.style.setProperty("--ten-pr-color", "#DBC4F0");
+    //   document.documentElement.style.setProperty("--shade", "#3a2b5c");
+    // }
   });
 
   return (
     <div className="app">
+      {/* BrowswerRouter for all routing */}
       <BrowserRouter>
+        {/* Created a page where all routes lies */}
         <AllRoutes theme={theme} setTheme={setTheme} />
       </BrowserRouter>
-      {/* <div id="top" onClick={handleToTop} className={`${isVisible ? "visible" : "invisible"}`}> */}
     </div>
   );
 }
--- src/pages/Routes/AllRoutes.jsx
+++ src/pages/Routes/AllRoutes.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "../Home/Home.jsx";
-import Content from "../Content/Content.jsx";
+import { Home, Content } from "../index.js";
 import { Subject } from "../../components/index.js";
 
 const AllRoutes = ({ theme, setTheme }) => {
+  // to access the location of url
   const location = useLocation();
-
+  // to get the details of selected card in a content page
   const [selectedCard, setSelectedCard] = useState(null);
   return (
     <AnimatePresence>
       <Routes location={location} key={location.pathname}>
+        {/* Home Route */}
         <Route path="/" element={<Home theme={theme} setTheme={setTheme} />} />
+        {/* Content Route */}
         <Route
           path="/content"
           element={
@@ -24,6 +26,7 @@ const AllRoutes = ({ theme, setTheme }) => {
             />
           }
         />
+        {/* Subject Route */}
         <Route
           path="/subject"
           element={<Subject selectedCard={selectedCard} />}
