f80353234ea4b4016a1c80e51714b915bc52cd78
Uttam Makwana <uttamakwana4503@gmail.com>
2023-07-30 10:03:20 +0530

    Working properly

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,16 @@ function App() {
         "#2F327D"
       );
       document.documentElement.style.setProperty("--ten-pr-color", "#545AE7");
+      document.documentElement.style.setProperty("--shade", "#d7dcf8");
     } else {
       document.documentElement.style.setProperty("--sixty-pr-color", "#2C1F4A");
-      document.documentElement.style.setProperty("--thirty-pr-color", "#F1F1F1");
-      document.documentElement.style.setProperty("--ten-pr-color", "#545AE7");
+      document.documentElement.style.setProperty(
+        "--thirty-pr-color",
+        "#fff"
+        );
+        document.documentElement.style.setProperty("--ten-pr-color", "#DBC4F0");
+        document.documentElement.style.setProperty("--shade", "#3a2b5c");
+
     }
   });
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import './footer.css'
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer mt-6">
       <div className="edu-logo">
-      <div className="logo">
+        <div className="logo">
           <div className="square"></div>
-          <h4>EduWise</h4>
+          <h4 id="footer-logo">EduWise</h4>
         </div>
         <div className="v-line" />
         <span>New Era of learning</span>
       </div>
       <div className="subscribe-letter">
-        <h5>Contact Us for any kind of <span>query</span></h5>
+        <h5>
+          Contact Us for any kind of <span>query</span>
+        </h5>
         <div className="form">
           <input type="text" placeholder="Your Email" />
           <button>Message</button>
